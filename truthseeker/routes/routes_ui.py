67afb7ac8f2369c50b2fa6649b6a2b78881a8dae
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2022-12-01 10:57:39 +0100

    Basic UI pages

--- truthseeker/routes/routes_ui.py
+++ truthseeker/routes/routes_ui.py
@@ -6,5 +6,18 @@ from truthseeker.logic import game_logic
 routes_ui = flask.Blueprint("ui", __name__)
 
 @routes_ui.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return flask.render_template("index.html")
+
+@routes_ui.route("/lobby/<game_id>")
+def lobby(game_id):
+    # rendered by the javascript client-side
+    return flask.render_template("lobby.html")
+
+@routes_ui.route("/solo")
+def solo():
+    return flask.render_template("game.html")
+
+@routes_ui.route("/multi")
+def multi():
+    return flask.render_template("game.html")
