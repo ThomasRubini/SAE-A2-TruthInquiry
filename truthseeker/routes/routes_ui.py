6d960222cccb7613791310c790c751d8a3f71649
AudricV <74829229+AudricV@users.noreply.github.com>
2023-01-02 19:14:09 +0100

    [Server] Add legal pages to existing routes

--- truthseeker/routes/routes_ui.py
+++ truthseeker/routes/routes_ui.py
@@ -9,6 +9,18 @@ routes_ui = flask.Blueprint("ui", __name__)
 def index():
     return flask.render_template("index.html")
 
+@routes_ui.route("/privacy")
+def privacy():
+    return flask.render_template("privacy.html")
+
+@routes_ui.route("/licenses")
+def licenses():
+    return flask.render_template("licenses.html")
+
+@routes_ui.route("/legal")
+def legal():
+    return flask.render_template("legal.html")
+
 @routes_ui.route("/lobby/<game_id>")
 def lobby(game_id):
     # rendered by the javascript client-side
