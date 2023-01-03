54d719065ba6b6bb7a5e579ad7222bb6674085c0
SIMAILA Djalim <djalimS.pro@outlook.fr>
2023-01-03 14:39:25 +0100

    added remote connection

--- data_persistance/data.py
+++ data_persistance/data.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy import engine as eg
+
 from tables import *
 
 from answer import ANSWER
@@ -10,16 +12,34 @@ from questions import QUESTIONS
 from reactions import REACTIONS
 from traits import TRAITS
 
+from secret import HOST, USER, PASS
+
+url_object = eg.URL.create(
+    "mariadb+pymysql",
+    username=USER,
+    password=PASS,
+    host=HOST,
+    port=6776,
+    database="truthInquiry",
+)
 
 # Create Engine and tables
-engine = create_engine("sqlite://", echo=True, future=True)
+engine = create_engine(url_object)
 Base.metadata.create_all(engine)
 
 with Session(engine) as session:
-    session.add_all(ANSWER)
+    print("adding locales")
     session.add_all(LOCALES)
-    session.add_all(NPCS)
+    print("adding places")
     session.add_all(PLACES)
-    session.add_all(QUESTIONS)
-    session.add_all(REACTIONS)
+    print("adding NPCS")
+    session.add_all(NPCS)
+    print("adding trait")
     session.add_all(TRAITS)
+    print("adding questions")
+    session.add_all(QUESTIONS)
+    print("adding answers")
+    session.add_all(ANSWER)
+    print("adding reaction")
+    session.add_all(REACTIONS)
+    session.commit()
