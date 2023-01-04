74f5c82a5de87af86a7005246c4c07b373c9aacb
AudricV <74829229+AudricV@users.noreply.github.com>
2023-01-04 10:03:27 +0100

    [Client] Add initial interactions on the game selection view
    These interactions are checks of nickname and room code validity, where it is
    relevant.
    
    Also set a maximum room code length to 20 characters, at least for now.

--- truthseeker/static/js/game_start_page.js
+++ truthseeker/static/js/game_start_page.js
@@ -29,5 +29,117 @@ function hideGameModeSelection() {
     document.getElementsByClassName("game_mode_selection")[0].classList.add("hidden");
 }
 
+/**
+ * Hide an error message on the first game_start_failed CSS element.
+ *
+ * <p>
+ * The element will be hidden by removing the hidden CSS class on the element.
+ * </p>
+ */
+function hideInvalidInputErrorMessage() {
+    document.getElementsByClassName("game_start_failed")[0].classList.add("hidden");
+}
+
+/**
+ * Show an error message on the first game_start_failed CSS element.
+ *
+ * <p>
+ * The current error message text will be replaced by the given message and the element will be
+ * shown, by removing the hidden CSS class on the element.
+ * </p>
+ *
+ * @param {boolean} errorMessage the error message to show
+ */
+function showInvalidInputErrorMessage(errorMessage) {
+    let gameStartFailedElement = document.getElementsByClassName("game_start_failed")[0];
+    gameStartFailedElement.textContent = errorMessage;
+    gameStartFailedElement.classList.remove("hidden");
+}
+
+/**
+ * Determine whether a nickname is invalid.
+ *
+ * <p>
+ * A nickname is invalid when it only contains spaces characters or is empty.
+ * </p>
+ *
+ * @returns whether a nickname is invalid
+ */
+function isNickNameInvalid() {
+    return document.getElementById("game_username").value.trim() == "";
+}
+
+/**
+ * Determine whether a room code is invalid.
+ *
+ * <p>
+ * A room code is invalid when it only contains spaces characters or is empty.
+ * </p>
+ *
+ * @returns whether a room code is invalid
+ */
+function isRoomCodeInvalid() {
+    return document.getElementById("game_room_code").value.trim() == "";
+}
+
+/**
+ * Determine whether nickname and/or room code inputs are valid.
+ *
+ * <p>
+ * The nickname validity is always checked, while the room code validity is checked only when
+ * checkRoomCode is true (because when creating a room or playing on a solo match, the room code is
+ * not used so checking it would be useless and would require a valid room code).
+ * </p>
+ *
+ * @param {boolean} checkRoomCode whether the room code input should be checked
+ * @returns whether the checked inputs are valid
+ */
+function areInputsValid(checkRoomCode) {
+    if (isNickNameInvalid()) {
+        showInvalidInputErrorMessage("Le nom saisi n'est pas valide.");
+        return false;
+    }
+
+    if (checkRoomCode && isRoomCodeInvalid())  {
+        showInvalidInputErrorMessage("Le code de salon saisi n'est pas valide.");
+        return false;
+    }
+
+    return true;
+}
+
+function startSoloGame() {
+    if (!areInputsValid(false)) {
+        return;
+    }
+
+    hideInvalidInputErrorMessage();
+
+    //TODO: code to start solo game
+}
+
+function createMultiPlayerRoom() {
+    if (!areInputsValid(false)) {
+        return;
+    }
+
+    hideInvalidInputErrorMessage();
+
+    //TODO: code to create multi player game
+}
+
+function joinMultiPlayerRoom() {
+    if (!areInputsValid(true)) {
+        return;
+    }
+
+    hideInvalidInputErrorMessage();
+
+    //TODO: code to join multi player game
+}
+
 document.getElementById("play_button").addEventListener("click", showGameModeSelection);
 document.getElementById("back_button").addEventListener("click", hideGameModeSelection);
+document.getElementById("start_solo_game_button").addEventListener("click", startSoloGame);
+document.getElementById("create_room_button").addEventListener("click", createMultiPlayerRoom);
+document.getElementById("join_room_button").addEventListener("click", joinMultiPlayerRoom);
