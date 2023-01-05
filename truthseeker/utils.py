c670cd36e19b27dd5b6bcfcf9e263fbffd30645b
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-05 15:07:34 +0100

    check if username is alphanumeric

--- truthseeker/utils.py
+++ truthseeker/utils.py
@@ -1,6 +1,8 @@
 def check_username(username):
     if not username:
         return False
+    if not username.isalnum():
+        return False
     if not username == username.strip():
         return False
     if not len(username) < 16:
