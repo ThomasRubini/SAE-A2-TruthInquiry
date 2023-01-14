71aedd3854c4c231168df110acabfbec721b17a8
SIMAILA Djalim <djalimS.pro@outlook.fr>
2023-01-14 12:36:15 +0100

    show npcs on interogation screen

--- truthseeker/static/js/game.js
+++ truthseeker/static/js/game.js
@@ -1,8 +1,23 @@
 var npcs_ids = []
 var gamedata = {}
 var button = ""
-async function showAnswerSelectionPanel() {
-    npcs_ids.forEach(async element => {
+
+function showInterogation(){
+    document.getElementsByClassName("interrogation")[0].classList.remove("hidden");
+}
+function hideInterogation(){
+    document.getElementsByClassName("interrogation")[0].classList.add("hidden");
+}
+
+function showEmotionAndCulpritChoices(){
+    document.getElementsByClassName("emotion_and_culprit_choices")[0].classList.remove("hidden");
+}
+function hideEmotionAndCulpritChoices(){
+    document.getElementsByClassName("emotion_and_culprit_choices")[0].classList.add("hidden");
+}
+
+function renderAnswerSelectionPanel() {
+    npcs_ids.forEach(element => {
         console.log(element);
         let suspect = document.createElement("div");
         suspect.classList.add("suspect");
@@ -21,11 +36,28 @@ async function showAnswerSelectionPanel() {
         img.classList.add("suspect_picture");
         img.src = "/api/v1/getNpcImage?npcid="+element; 
         suspect.appendChild(img);
-        let button = document.getElementById("mainButton");
+        let button = document.getElementById("culpritButton");
         let button_clone = button.cloneNode(true);
         button_clone.classList.remove("hidden");
         suspect.appendChild(button_clone);
         document.getElementById("culprits_choices").appendChild(suspect);
+    });
+}
+
+function renderInterogation(){
+    npcs_ids.forEach(element => {
+        let suspect = document.createElement("div");
+        suspect.classList.add("suspect");
+
+        let img = document.createElement('img');
+        img.classList.add("suspect_picture");
+        img.src = "/api/v1/getNpcImage?npcid="+element; 
+        suspect.appendChild(img);
+        let button = document.getElementById("interogationButton");
+        let button_clone = button.cloneNode(true);
+        button_clone.classList.remove("hidden");
+        suspect.appendChild(button_clone)
+        document.getElementById("interrogation_suspects").appendChild(suspect);
     });
 }
 
@@ -45,13 +77,19 @@ function initSock(){
     });
 }
 
-function setGameData(){
+async function setGameData(){
     data = {};
-    response = makeAPIRequest("getGameData");
-    response.then((value) => {
-        gamedata = value["gamedata"];
-        npcs_ids = Object.keys(gamedata["npcs"]);
-    })
+    response = await makeAPIRequest("getGameData");
+    gamedata = response["gamedata"];
+    npcs_ids = Object.keys(gamedata["npcs"]);
 }
 
-setGameData()
+async function initGame(){
+    await setGameData();
+    //initSock();
+    renderAnswerSelectionPanel();
+    renderInterogation();
+    showInterogation();
+}
+
+initGame();
