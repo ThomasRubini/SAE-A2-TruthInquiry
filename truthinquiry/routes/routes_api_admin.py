f3fa09e25f69a549ca9022cdf2d943a2f7be66bb
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-03-11 18:15:48 +0100

    implement get_questions() API

--- truthinquiry/routes/routes_api_admin.py
+++ truthinquiry/routes/routes_api_admin.py
@@ -9,4 +9,26 @@ routes_api_admin = flask.Blueprint("api_admin", __name__)
 
 @routes_api_admin.route("/getQuestions", methods=["GET", "POST"])
 def get_questions():
-    pass
+    lang = flask.request.values.get("lang")
+    if lang is None:
+        return {"error": 1, "msg": "lang not set"}
+
+
+    results = db.session.execute(
+        select(QuestionType, Locale)
+        .join(Locale)
+        .filter(Locale.LANG==lang)
+        .order_by(QuestionType.QUESTION_TYPE_ID)
+    )
+
+    data = []
+    old_question_type_id = None
+
+    for question_type, locale in results:
+        if question_type.QUESTION_TYPE_ID != old_question_type_id:
+            old_question_type_id = question_type.QUESTION_TYPE_ID
+            data.append([])
+
+        data[-1].append({"text": locale.TEXT})
+    
+    return data
