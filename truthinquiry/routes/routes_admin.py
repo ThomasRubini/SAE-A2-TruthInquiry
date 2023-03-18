92c6d089e790ef878f2e2570134ea25133e1618b
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-03-18 15:48:08 +0100

    add back office npc view

--- truthinquiry/routes/routes_admin.py
+++ truthinquiry/routes/routes_admin.py
@@ -1,6 +1,18 @@
 import flask
 
+from truthinquiry.ext.database.models import *
+from truthinquiry.ext.database.fsa import db
+
 routes_admin = flask.Blueprint("admin", __name__)
+
+def get_or_empty(obj, key):
+    if obj == None:
+        return ""
+    else:
+        return getattr(obj, key)
+
+
+
 
 @routes_admin.route("/")
 def index():
@@ -8,7 +20,23 @@ def index():
 
 @routes_admin.route("/npc/<npc_id>")
 def npc(npc_id):
-    return flask.render_template("admin/npc.html")
+    if npc_id == "new":
+        return flask.render_template("admin/npc.html", npc={})
+    else:
+        npc_obj = db.session.get(Npc, npc_id)
+
+        npc_answers = []
+        for answer_type in npc_obj.ANSWERS:
+            answer_list = [answer.TEXT for answer in answer_type.LOCALE.TEXTS]
+            npc_answers.append(answer_list)
+        
+        npc_dict = {
+            "name": npc_obj.LOCALE.TEXTS[0].TEXT,
+            "img": npc_obj.NPC_ID,
+            "answers": npc_answers,
+        }
+
+        return flask.render_template("admin/npc.html", npc=npc_dict)
 
 @routes_admin.route("/questions")
 def questions():
